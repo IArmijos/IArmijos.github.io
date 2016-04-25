ae289dabb6e517d416c2154cb7017dd86e099507
Israel Armijos <armijos.israel@gmail.com>
2016-04-25 08:59:11 -0400

    Maze Explorer Prototype 1
    This is the first prototype to my final game Maze Explorer. In this
    prototype, I've testing out the walking code for the character and the
    raycasting code for the character to aim and fire. It is still
    incomplete. Please not that the basic shape and level layout isn't fully
    completed and will be changed in the final version. Thank you.

--- GD205HW/HW7/HW# 7/Assets/Scripts/Raycasting.cs
+++ GD205HW/HW7/HW# 7/Assets/Scripts/Raycasting.cs
@@ -25,11 +25,11 @@ public class Raycasting : MonoBehaviour {
 		GetComponent<Rigidbody> ();
 		Ray myRay = Camera.main.ScreenPointToRay (Input.mousePosition);
 		Camera.main.transform.LookAt (myRay.direction);
-		Debug.DrawRay (myRay.origin, myRay.direction * 1000, Color.red);
+		Debug.DrawRay (myRay.origin, myRay.direction * 5000, Color.red);
 
 		RaycastHit rayhit = new RaycastHit ();
 
-		if (Physics.Raycast (myRay, out rayhit, 1000f) && Input.GetMouseButtonDown(0)){
+		if (Physics.Raycast (myRay, out rayhit, 5000f) && Input.GetMouseButtonDown(0)){
 			Debug.Log("You hit " + rayhit.transform);
 			Instantiate(obj, rayhit.point, Random.rotation);
 			Destroy (rayhit.collider.gameObject);
