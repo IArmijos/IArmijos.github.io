508f9a019e28565d49c8c249fcd2dd100397d9bb
Israel Armijos <armijos.israel@gmail.com>
2016-05-02 09:49:43 -0400

    Prototype 2
    This second prototype of my maze explorer game will focus on the
    graphical aspects of the game. That includes background and walls.
    Character sprites and 3-D models will come soon

---
+++ Maze Explorer/mazeGame/Assets/Scripts/HUDText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDText : MonoBehaviour {
+	public GameObject scoreText;
+
+	int playerScore = 0;
+
+	// Use this for initialization - used to set the score found in the player's camera. The 3D Text must be used as a child to
+	// the player character.
+	void Start () {
+		scoreText.GetComponent<TextMesh> ().color = Color.white;
+		scoreText.GetComponent<TextMesh> ().text = "Score Counter " + playerScore;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
